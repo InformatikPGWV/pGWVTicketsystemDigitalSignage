6ccfe68e239fffad2d9b5a0cff2e0c1ca393cd26
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2022-09-21 21:12:56 +0200

    added basic substring method

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="de" data-theme="dark">
+<html lang="de" data-theme="light">
 
 <head>
     <meta charset="UTF-8">
@@ -8,89 +8,13 @@
 
     <title>PGWV Fehlersystem</title>
 
-
-    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Open Graph & Social Media Tags ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-
-
-    <!-- === Meta Tags for Social Media and SEO === -->
-    <!-- = Info: https://ogp.me/#types = -->
-    <!-- = GENERAL = -->
-    <!-- <meta property="og:site_name" content="TEMPLATE" />
-    <meta property="og:title" content="TEMPLATE" />
-    <meta property="og:description" content="TEMPLATE" />
-    <meta property="og:type" content="TEMPLATE" />
-    <meta property="og:url" content="TEMPLATE" />
-    <meta property="og:image" content="TEMPLATE" /> -->
-    <!-- = TWITTER = -->
-    <!-- <meta name="twitter:title" content="TEMPLATE" />
-    <meta name="twitter:description" content="TEMPLATE" />
-    <meta name="twitter:image" content="TEMPLATE" />
-    <meta name="twitter:card" content="TEMPLATE" /> -->
-
-
-    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Design ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-
-
-    <!-- = THEME = -->
-    <!-- <meta name="theme-color" content="#16a34a" /> -->
-
-
-    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ JavaScript Frameworks ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-
-
-    <!-- === JQuery (https://releases.jquery.com) === -->
-    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> -->
-
-
-    <!-- === py-script (https://pyscript.net) === -->
-    <!-- <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
-    <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
-    <py-env>
-        -
-    </py-env> -->
-
-
-    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Design Frameworks ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-
-
-    <!-- === Tailwind CSS (https://tailwindcss.com/docs/customizing-colors#) === -->
-    <!-- <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-        tailwind.config = {
-            corePlugins: {
-                preflight: true,
-            },
-        }
-    </script> -->
-
-
-    <!-- === DaisyUI (https://daisyui.com/components) === -->
-    <!-- Added Via Tailwind -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/daisyui@2.17.0/dist/full.css" rel="stylesheet" type="text/css" /> -->
-
-
-    <!-- === Material Design (https://material.io/components?platform=web & https://m3.material.io/components/buttons/implementation/web ; https://material.io/develop/web/getting-started) === -->
-    <!-- <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
-    <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"> -->
-
-
-    <!-- === 98.css (https://jdan.github.io/98.css/#components) === -->
-    <!-- <link rel="stylesheet" href="https://unpkg.com/98.css"> -->
-
-
-    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Customization ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-
-    <link rel="stylesheet" href="indexOutput.css">
-
     <!-- === CSS === -->
     <!-- <style>
-        
-    </style> -->
+        </style> -->
+    <link rel="stylesheet" href="indexOutput.css">
 
     <!-- === RELOAD === -->
-    <!-- <meta http-equiv="refresh" content="10; url=#"> -->
+    <meta http-equiv="refresh" content="1800; url=#">
 
     <!-- === PHP === -->
     <?php
@@ -112,20 +36,20 @@
 <body class="font-sans">
 
     <!-- NAVBAR -->
-    <div class="text-4xl font-bold text-white bg-red-600 pl-5 pt-3 pb-5 ">
-        <h1 style="width: 100vw;">PGWV Fehlersystem <span class="inlineRight mr-4"><?php echo $fehlerzahl ?> Fehler</span>
+    <div class="sticky z-50 text-4xl font-bold text-white bg-red-600 pl-5 pt-3 pb-5">
+        <h1 style="width: 100vw;">PGWV Fehlersystem <span class="inlineRight mr-16"><?php echo $fehlerzahl ?> Fehler</span>
         </h1>
     </div>
 
     <!-- TABELLE -->
-    <table class="table">
+    <table class="table mt-20 w-full z-1">
         <thead>
             <tr>
-                <th class="text-center" style="width: 7.5%;">Raum</th>
-                <th class="text-center" style="width: 55%">Fehler</th>
-                <th class="text-center" style="width: 22.5%;">Status</th>
-                <!-- <th class="text-center" style="width: 12.5%;">Melder</th> -->
-                <th class="text-center" style="width: 15%;">Datum</th>
+                <th class="text-center z-1" style="width: 7.5%; max-width: 7.5%;">Raum</th>
+                <th class="text-center z-1" style="width: 55%; max-width: 55%;">Fehler</th>
+                <th class="text-center z-1" style="width: 22.5%; max-width: 22.5%;">Status</th>
+                <!-- <th class="text-center z-1" style="width: 12.5%; max-width: 12.5%;">Melder</th> -->
+                <th class="text-center z-1" style="width: 15%; max-width: 15%;">Datum</th>
             </tr>
         </thead>
         <tbody id="problemTable">
@@ -135,9 +59,9 @@
             ?>
                 <tr id="<?php echo $currentId; ?>">
                     <td class="text-center"><?php echo $problem['raum']; ?></td>
-                    <td class="text-center"><b><?php echo $problem['kategorie']; ?></b> <br> <?php echo $problem['problembeschreibung']; ?></td>
+                    <td class="text-center"><b><?php echo $problem['kategorie']; ?></b> <br> <?php echo substr($problem['problembeschreibung'], 0, 100); ?></td>
                     <td class="text-center"><?php echo $problem['status']; ?><br></td>
-                    <!-- <td class="text-center"><?php //echo $problem['melder']; 
+                    <!-- <td class="text-center"><?php //echo $problem['melder']; S
                                                     ?></td> -->
                     <td class="text-center"><?php echo $problem['datum']; ?></td>
                 </tr>
@@ -150,9 +74,6 @@
     </table>
 
 
-
-
-
     <!-- ================================================================================================================== -->
 
     <!-- TODO:
@@ -162,7 +83,7 @@
 
 
     <!-- === JavaScript === -->
-    <script>
+    <!-- <script>
         // $(document).ready(function() {
         //     // jQuery methods go here...
         // });
@@ -175,9 +96,7 @@
         // ======================================================================================
 
         function scrollDone() {
-
             setTimeout(() => {
-                // Mach was
                 window.scrollTo({
                     top: 0,
                     behavior: "smooth"
@@ -218,7 +137,7 @@
             isNotScrolling = false;
             scrollDownUntilEndOfPage();
         }, 5000);
-    </script>
+    </script> -->
 
 </body>
 
